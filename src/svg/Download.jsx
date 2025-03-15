19b8b2818ce8aa46ba32218bb87394141775e80b
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-15 20:59:15 +0500

    added pdf file resume

--- src/svg/Download.jsx
+++ src/svg/Download.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Download() {
   return (
     <a
-      href="/cv.pdf"
-      download="Aziz_CV.pdf"
+      href="/cv.pdf.pdf"
+      download="Azizulloh_Resume.pdf"
       className="flex items-center px-1 py-2 md:px-5 md:py-3 text-[9px] w-full md:text-[16px] bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 active:scale-95"
     >
       📄 Download CV
