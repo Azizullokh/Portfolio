e08ba142e3cacb7ae5ddda884a36ef27c8f69dbf
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-04-01 13:25:20 +0500

    fixed issues

--- src/svg/Download.jsx
+++ src/svg/Download.jsx
@@ -5,7 +5,7 @@ function Download() {
   const { translation } = useLanguageStore();
   return (
     <a
-      href="/cv.pdf.pdf"
+      href="/cvv.pdf"
       download="Azizulloh_Resume.pdf"
       className="flex items-center px-1 py-2 md:px-5 md:py-3 text-[9px] w-full md:text-[16px] bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 active:scale-95"
     >
